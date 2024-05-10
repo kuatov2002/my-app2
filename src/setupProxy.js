b8c97ee787ab2fdbe8934715bd005a01fd5edaa0
kuatov2002 <71309258+kuatov2002@users.noreply.github.com>
2024-05-10 09:39:17 +0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080', // Replace with your API server URL
+      target: 'http://localhost:5860', // Replace with your API server URL
       changeOrigin: true,
       pathRewrite: {
         '^/api': '', // Remove '/api' from the request URL
