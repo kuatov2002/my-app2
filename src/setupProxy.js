cdec08590c293c4ebb05f7fffa33f844f7f8a63d
kuatov2002 <71309258+kuatov2002@users.noreply.github.com>
2024-06-09 15:17:18 +0500

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://algoanalysesdeploym-production.up.railway.app', // Replace with your API server URL
+      target: 'http://algoanalysesdeploym-production-2364.up.railway.app', // Replace with your API server URL
       changeOrigin: true,
       pathRewrite: {
         '^/api': '', // Remove '/api' from the request URL
